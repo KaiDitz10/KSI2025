9c74741f1c43d04d78f9f2d8af50aa9062c4c9fa
KaiDitz10 <adityatampubolon10@gmail.com>
2025-11-07 17:13:02 +0700

    Simpan perubahan sebelum pindah branch

--- tambah_data.php
+++ tambah_data.php
@@ -1,10 +1,16 @@
-echo "<?php
+<?php
 include 'koneksi.php';
-\$nama = 'Aditya';
-\$sql = \"INSERT INTO users (nama) VALUES ('\$nama')\";
-if(mysqli_query(\$koneksi, \$sql)){
-  echo 'Data berhasil ditambahkan!';
+
+$nama = "Aditya";
+
+$sql = "INSERT INTO users (nama) VALUES ('$nama')";
+
+if (mysqli_query($koneksi, $sql)) {
+    echo "Data berhasil ditambahkan!";
 } else {
-  echo 'Gagal menambah data: ' . mysqli_error(\$koneksi);
+    echo "Gagal menambah data: " . mysqli_error($koneksi);
 }
-?>" > tambah_data.php
+
+// Tutup koneksi
+mysqli_close($koneksi);
+?>
